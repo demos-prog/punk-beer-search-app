ded133b36d47342f1ab58e5f528e4da15f3d3615
Дмитрий <demosne@gmail.com>
2021-08-20 09:51:31 +0300

    qwe

--- src/components/homeCont.jsx
+++ src/components/homeCont.jsx
@@ -5,8 +5,15 @@ export default function HomeCont(props) {
   let list = props.arr.map((item, ind) => {
     return (
       <div className="item" key={nanoid()}>
-        <div className="image">
-          <img src={item.image_url} alt={ind}></img>
+        <div className="item__favor"></div>
+        <div className="item__content">
+          <div className="item__image">
+            <img src={item.image_url} alt={ind}></img>
+          </div>
+          <div className="item__text">
+            <div className="item__name">{item.name}</div>
+            <div className="item__txt">{item.description}</div>
+          </div>
         </div>
       </div>
     );
