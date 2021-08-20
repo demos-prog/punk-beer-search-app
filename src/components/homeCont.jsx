de308176b40f74a3e2ad59bd8120486361387cde
Дмитрий <demosne@gmail.com>
2021-08-20 11:14:21 +0300

    ui

--- src/components/homeCont.jsx
+++ src/components/homeCont.jsx
@@ -5,7 +5,9 @@ export default function HomeCont(props) {
   let list = props.arr.map((item, ind) => {
     return (
       <div className="item" key={nanoid()}>
-        <div className="item__favor"></div>
+        <div className="item__favor_wrapper">
+          <button className="item__favor">Add to favor</button>
+        </div>
         <div className="item__content">
           <div className="item__image">
             <img src={item.image_url} alt={ind}></img>
